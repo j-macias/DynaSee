e3436c6ea4609f984f0c8e337aa82b38447b27e9
jmacias <jmacias@uw.edu>
2017-04-29 16:44:41 -0700

    Fullscreen and landscape locked

--- app/src/main/java/edu/washington/ischool/pjjj/dynasee/MainActivity.java
+++ app/src/main/java/edu/washington/ischool/pjjj/dynasee/MainActivity.java
@@ -36,4 +36,17 @@ public class MainActivity extends AppCompatActivity implements OnBottomNavigatio
 //    public void rb1(View view){
 //        mKqwBottomNavigation.setBottomNavigationClick(1);
 //    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
